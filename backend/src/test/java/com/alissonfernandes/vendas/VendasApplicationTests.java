5520db3432f3a59575926441f54e8510849695b9
Alisson <alisson.alencar98@hotmail.com>
2021-05-03 19:31:38 -0300

    Project created

---
+++ backend/src/test/java/com/alissonfernandes/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissonfernandes.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
