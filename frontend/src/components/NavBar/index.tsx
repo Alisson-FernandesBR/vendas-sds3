39d371e47c73f1b41d6cf0b28170c3f29b795e6b
Alisson <alisson.alencar98@hotmail.com>
2021-05-08 14:09:48 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
     return (
         <div>
             <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
                 <div className="container">
                     <nav className="my-2 my-md-0 mr-md-3">
-                        <h2>AlissonFernandes</h2>
+                        <Link className="text-primary" to="/">
+                            <h2>AlissonFernandes</h2>
+                        </Link>
                     </nav>
                 </div>
             </div>
